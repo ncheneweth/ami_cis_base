9e31d4e2679723a2e38f5e4c9f9dbe289e139136
Ranbir Chawla <rchawla@thoughtworks.com>
2017-05-19 18:25:52 -0600

    #419 cleaned up syntax errors after testing and moved template files to the root, less issus with ansible roles this way

--- step1.py
+++ step1.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import sys
 import subprocess
+import os
+
 
 if __name__ == "__main__":
     print ("going to run bootstrap 1")
-    # try:
-        subprocess.run(['/usr/local/bin/packer','build',os.getcwd() + '/builders/bootstrap1.json'], check=True)
+    try:
+        subprocess.run(['/usr/local/bin/packer','build',os.getcwd() + '/bootstrap1.json'], check=True)
     except subprocess.CalledProcessError as err:
         print('ERROR:', err)
+        sys.exit(2)
--- step2.py
+++ step2.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
     latest=list(filter(lambda build: build['packer_run_uuid'] == latest_uuid, builds))
     if(len(latest) > 0):
         artifact = latest[0]['artifact_id'].split(':')
-        print('going to build bootstrap 3 in region:' + artifact[0] + ' with artifact:' + artifact[1])
+        print('going to build bootstrap 2 in region:' + artifact[0] + ' with artifact:' + artifact[1])
     else:
-        print("failed to find previous ami in bootstrap2-manifest.json")
-
+        print("failed to find previous ami in bootstrap1-manifest.json")
+        sys.exit(2)
     try:
-        subprocess.run(['/usr/local/bin/packer','build','-var','region='+artifact[0],'-var','parent_ami_id='+artifact[1],os.getcwd() + '/builders/bootstrap2.json'], check=True)
+        subprocess.run(['/usr/local/bin/packer','build','-var','region='+artifact[0],'-var','parent_ami_id='+artifact[1],os.getcwd() + '/bootstrap2.json'], check=True)
     except subprocess.CalledProcessError as err:
         print('ERROR:', err)
+        sys.exit(2)
